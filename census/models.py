b60bda42f39389f8d62ab2d1b522051a0baddf51
Ubuntu <ubuntu@ip-10-0-0-40.us-west-2.compute.internal>
2022-12-10 13:56:38 +0000

    Added draft Models

--- census/models.py
+++ census/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+class Geometry(models.Model):
+    code = models.CharField(max_length=100)
+    geom = models.MultiPolygonField()
+
+    def __str__(self):
+        return self.name
+
+class Field(models.Model):
+    code = models.CharField(max_length=100)
+    geometry = models.ManyToManyField(Geometry, through='Data')
+
+    def __str__(self):
+        return self.name
+
+class GeometryData(models.Model):
+    geometry = models.ForeignKey(Geometry, on_delete=models.CASCADE)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    data = models.DecimalField() #TODO: find field scale
--- storage/models.py
+++ storage/models.py
@@ -1,13 +1,43 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
 class Store(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
-    instrument = models.CharField(max_length=100)
+    geom = models.PointField()
 
-class Album(models.Model):
-    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class StoreIso(models.Model):
+    time = models.IntegerField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    geom = models.MultiPolygonField()
+
+    def __str__(self):
+        return self.name
+
+class StoreIsoWeighting(models.Model):
+    name = models.CharField(max_length=50)
+    storeiso = models.ManyToManyField(StoreIso, through='StoreIsoWeight')
+
+    def __str__(self):
+        return self.name
+
+class StoreIsoWeight(models.Model):
+    storeiso = models.ForeignKey(StoreIso, on_delete=models.CASCADE)
+    storeisoweighting = models.ForeignKey(StoreIsoWeighting, on_delete=models.CASCADE)
+    weight = models.FloatField()
+
+
+class Model(models.Model):
+    name = models.CharField(max_length=50)
+    field = models.ManyToManyField('census.Field', through='Coefficient')
+
+    def __str__(self):
+        return self.name
+
+class Coefficient(models.Model):
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
+    field = models.ForeignKey('census.Field', on_delete=models.CASCADE)
+    value = models.FloatField()
